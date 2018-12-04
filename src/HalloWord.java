135a77675baec06b835f071f91d28b7b82cea0bd
MrBigdogge <walker2388685730@163.com>
2018-12-04 19:44:31 +0800

    first commit

---
+++ src/HalloWord.java
@@ -0,0 +1,14 @@
+/**
+ * @author shksart
+ * @date 2018/12/04 -  19:38
+ */
+public class HalloWord {
+
+    public static void main(String[] args) {
+
+        System.out.println("halloword!!!");
+
+
+    }
+
+}
